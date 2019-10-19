dcb0ac2fad578046310561443fd8c7f12a2516f0
tao <821869740@qq.com>
2019-10-19 23:47:12 +0800

    fix

---
+++ build/CMakeFiles/onboard.dir/DependInfo.cmake
@@ -0,0 +1,29 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/tao/Desktop/onboard/src/camera_node.cpp" "/home/tao/Desktop/onboard/build/CMakeFiles/onboard.dir/src/camera_node.cpp.o"
+  "/home/tao/Desktop/onboard/src/clock.cpp" "/home/tao/Desktop/onboard/build/CMakeFiles/onboard.dir/src/clock.cpp.o"
+  "/home/tao/Desktop/onboard/src/control_node.cpp" "/home/tao/Desktop/onboard/build/CMakeFiles/onboard.dir/src/control_node.cpp.o"
+  "/home/tao/Desktop/onboard/src/log_node.cpp" "/home/tao/Desktop/onboard/build/CMakeFiles/onboard.dir/src/log_node.cpp.o"
+  "/home/tao/Desktop/onboard/src/main.cpp" "/home/tao/Desktop/onboard/build/CMakeFiles/onboard.dir/src/main.cpp.o"
+  "/home/tao/Desktop/onboard/src/socket_nodes.cpp" "/home/tao/Desktop/onboard/build/CMakeFiles/onboard.dir/src/socket_nodes.cpp.o"
+  "/home/tao/Desktop/onboard/src/topics.cpp" "/home/tao/Desktop/onboard/build/CMakeFiles/onboard.dir/src/topics.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../include"
+  "/usr/local/include/opencv4"
+  "/usr/include/mavsdk"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
