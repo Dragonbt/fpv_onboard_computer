f08c6425c18032b64c135379eab27d73c11ddfa9
tao <821869740@qq.com>
2019-11-11 23:39:21 +0800

    roll test 3

--- src/detector.cpp
+++ src/detector.cpp
@@ -336,7 +336,7 @@ bool CircleDetector::run(Mat image, Rect2f &rec, float& confidence)
         edgeDetection(est_roi, edges, dx, dy, 300);
         edges = edges & mask;
         confidence = circleDetect( edges, dx, dy, rec, 5, r - 5, est_rec.width / 2);
-        if ( confidence < 2.0f ){
+        if ( confidence < 1.0f ){
             if(type == DETECT_AND_TRACK)
             {
                 status = LOCAL_ESTIMATE;
--- src/mission.cpp
+++ src/mission.cpp
@@ -323,7 +323,7 @@ void testLoop(shared_ptr<Telemetry> telemetry, shared_ptr<Offboard> offboard, Fi
 		case SAFE_QUIT_COMMAND:
 			//remotePrint("[WARNIGN]: LANDING!");
 			thrust = altitude_thrust_control.landing();
-			input_attitude = {0.0f, 0.0f, attitude.yaw_deg, thrust};
+			input_attitude = {0.0f, 5.0f, attitude.yaw_deg, thrust};
 			offbCtrlAttitude(offboard, input_attitude);
 			break;
 		case FORCE_QUIT_COMMAND:
@@ -362,12 +362,12 @@ void testLoop(shared_ptr<Telemetry> telemetry, shared_ptr<Offboard> offboard, Fi
 			if (latest<DetectionResult>(target_topic, target_timestamp, target, target_mtx) && timestampf() - target_timestamp < 30)
 			{
 				remotePrint("VISION CONTROL!");
-				status = VISION_CONTROL_MODE;
-				vision_roll_thrust_control.angleOffset(roll_deg, thrust, target, position_ned, velocity_body, attitude, period_ms);
-				//status = VISION_CONTROL_MODE_YAW;
-				// vision_roll_thrust_control.angleOffset_Yaw(yaw_deg, thrust, target, position_ned, velocity_body, attitude, period_ms);
-				// vision_roll_thrust_control.braking(roll_deg, pitch_deg, thrust_n, position_ned, velocity_body, attitude, period_ms);
-				input_attitude = {roll_deg, -2.0f, yaw_deg, thrust};
+				// status = VISION_CONTROL_MODE;
+				// vision_roll_thrust_control.angleOffset(roll_deg, thrust, target, position_ned, velocity_body, attitude, period_ms);
+				status = VISION_CONTROL_MODE_YAW;
+				vision_roll_thrust_control.angleOffset_Yaw(yaw_deg, thrust, target, position_ned, velocity_body, attitude, period_ms);
+				vision_roll_thrust_control.braking(roll_deg, pitch_deg, thrust_n, position_ned, velocity_body, attitude, period_ms);
+				input_attitude = {roll_deg, -5.0f, yaw_deg, thrust};
 				offbCtrlAttitude(offboard, input_attitude);
 				fail_cnt = 0;
 			}
@@ -414,7 +414,7 @@ void testLoop(shared_ptr<Telemetry> telemetry, shared_ptr<Offboard> offboard, Fi
 				}
 			}
 			cout << "yaw_deg_sp: " << yaw_deg << endl;
-			input_attitude = {roll_deg, -2.0f, yaw_deg, thrust};
+			input_attitude = {roll_deg, -5.0f, yaw_deg, thrust};
 			offbCtrlAttitude(offboard, input_attitude);
 			break;
 		case VISION_CONTROL_MODE:
